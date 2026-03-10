6ee6451b10ff0c26a3f9561360a376def708ef36
Emil Larsen <emilvl@stud.ntnu.no>
2026-03-10 09:50:39 +0100

    loan validator

--- routers/api.v1.router.js
+++ routers/api.v1.router.js
@@ -2,35 +2,53 @@ import express from "express";
 import uController from "../controllers/v1.user.controller.js";
 import bController from "../controllers/v1.book.controller.js";
 
-import { validate } from "../validators/v1.validate.js"
+import { validate } from "../validators/v1.validate.js";
 
 import userValidator from "../validators/v1.user.validator.js";
 const apiV1Router = express.Router();
 
-
 // Assigning handlers to routes
 apiV1Router.get("/users", uController.getAllUsers);
-apiV1Router.get("/users/:uid", userValidator.validateUid(), validate, uController.getUser);
-apiV1Router.get("/users/:uid/loans", userValidator.validateUid(), validate, uController.getLoans);
+apiV1Router.get(
+  "/users/:uid",
+  userValidator.validateUid(),
+  validate,
+  uController.getUser,
+);
+apiV1Router.get(
+  "/users/:uid/loans",
+  userValidator.validateUid(),
+  validate,
+  uController.getLoans,
+);
 
-apiV1Router.post("/users", validate,  uController.createUser) // Create a new user
-apiV1Router.put("/users/:uid", userValidator.validateUid(), validate, uController.updateUser) // Updating a specific user
+apiV1Router.post("/users", validate, uController.createUser); // Create a new user
+apiV1Router.put(
+  "/users/:uid",
+  userValidator.validateUid(),
+  validate,
+  uController.updateUser,
+); // Updating a specific user
 
-apiV1Router.post("/users/:uid/loans", userValidator.validateUid(), validate, uController.loanBook); // Adding a book to a list of loaned books to a specific user
-apiV1Router.delete("/users/:uid/loans/:loanid", userValidator.validateUid(), validate, uController.unloanBook); // removing a book from the list of loaned books for a specific user
-
-
-
-
+apiV1Router.post(
+  "/users/:uid/loans",
+  userValidator.validateLoanId(),
+  validate,
+  uController.loanBook,
+); // Adding a book to a list of loaned books to a specific user
+apiV1Router.delete(
+  "/users/:uid/loans/:loanid",
+  userValidator.validateUid(),
+  userValidator.validateLoanId(),
+  validate,
+  uController.unloanBook,
+); // removing a book from the list of loaned books for a specific user
 
 apiV1Router.get("/books", bController.getAllBooks);
 apiV1Router.get("/books/:bid", bController.getBook);
 apiV1Router.get("/books/:loans", bController.getLoanedBook);
 
-apiV1Router.post("/books", bController.createBook) // Create a new book
-apiV1Router.put("/books/:bid", bController.updateBook) // Updating a specific book
-
-
+apiV1Router.post("/books", bController.createBook); // Create a new book
+apiV1Router.put("/books/:bid", bController.updateBook); // Updating a specific book
 
 export default apiV1Router;
-
--- validators/v1.user.validator.js
+++ validators/v1.user.validator.js
@@ -1,15 +1,26 @@
 import { param, body } from "express-validator";
 import { checkUserExist } from "../services/v1.users.services.js";
-export function validateUid(){
-    return [
-        param("uid")
-            .isInt({ min: 0, max: Number.MAX_SAFE_INTEGER})
-            .withMessage("User IDs is supposed to be integers larger than 0!")
-            .toInt() // all follow up functions will receive uid as a integer
-            .custom(checkUserExist)
-    ];
+export function validateUid() {
+  return [
+    param("uid")
+      .isInt({ min: 0, max: Number.MAX_SAFE_INTEGER })
+      .withMessage("User IDs is supposed to be integers larger than 0!")
+      .bail() // If the checks before this in the validation chain fail, we quit! (aka do not run the check below)
+      .toInt() // all follow up functions will receive uid as a integer
+      .custom(checkUserExist),
+  ];
+}
+
+export function validateLoanId() {
+  return [
+    param(":loanid")
+      .isInt({ min: 0, max: Number.MAX_SAFE_INTEGER })
+      .withMessage("Loan IDs is supposed to be integers larger than 0!")
+      .toInt()
+  ];
 }
 
 export default {
-    validateUid
-}
+  validateUid,
+  validateLoanId
+};
